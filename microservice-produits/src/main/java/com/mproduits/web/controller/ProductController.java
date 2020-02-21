b1a4ff67697443436910a75153f5112e965f6ef9
vinsbld <vincent.blndel@gmail.com>
2020-02-21 17:07:38 +0100

    projectIsFinish

--- microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
+++ microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.mproduits.configurations.ApplicationPropertiesConfiguration;
 import com.mproduits.dao.ProductDao;
 import com.mproduits.model.Product;
 import com.mproduits.web.exceptions.ProductNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,8 @@ import java.util.Optional;
 
 @RestController
 public class ProductController {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     ProductDao productDao;
@@ -30,6 +34,8 @@ public class ProductController {
         if(products.isEmpty()) throw new ProductNotFoundException("Aucun produit n'est disponible à la vente");
 
         List<Product> listeLimitee = products.subList(0, appProperties.getLimiteDeProduits());
+
+        log.info("Récupération de la liste des produits");
 
         return listeLimitee;
 
---
+++ zuul-server/src/main/java/com/mcommerce/zuulserver/filters/LogFilter.java
@@ -0,0 +1,42 @@
+package com.mcommerce.zuulserver.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class LogFilter extends ZuulFilter {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public String filterType() {
+        return "pre"; //post
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    //le filtre doit il s'executé ou pas
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    //la logique du filtre est à mettre dans run
+    @Override
+    public Object run() throws ZuulException {
+
+        HttpServletRequest req = RequestContext.getCurrentContext().getRequest();
+         log.info("*** Requête interceptée "+ req.getRequestURL());
+
+        return null;
+    }
+}
